f40085e7fa1635e630a54b64889025064beb358c
xRossKoh <e0543512@u.nus.edu>
2021-08-20 17:37:01 +0800

    Commit to pull

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 public class Main {
-    static int[] amounts = new int[100];
-    static String[] borrowers = new String[100];
-    static boolean[] isPaid = new boolean[100];
-    static double[] interestRates = new double[100];
+
+    static Loan[] loans = new Loan[100];
+
     static int loansCount = 0;
 
     public static void addLoan(String borrower, int amount, double interestRate) {
@@ -14,8 +13,8 @@ public class Main {
 
     public static void setAsPaid(String borrower) {
         for (int i = 0; i < loansCount; i++) {
-            if (borrowers[i].equals(borrower)) {
-                isPaid[i] = true;
+            if (loans[i].getBorrower().equals(borrower)) {
+                loans[i].setPaid();
             }
         }
     }
